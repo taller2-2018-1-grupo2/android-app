4b1d2f85bf7db6266e72bc6a11f11c3501247dbf
gmerino92 <merino.gonza@gmail.com>
2018-05-12 04:37:09 -0300

    First attempt at Profile activity and some mods to Home activity.

--- app/src/main/java/stories/app/activities/HomeActivity.java
+++ app/src/main/java/stories/app/activities/HomeActivity.java
@@ -1,7 +1,11 @@
 package stories.app.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import stories.app.R;
 
@@ -10,6 +14,27 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.profile) {
+            Intent navigationIntent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(navigationIntent);
+            return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }

7d4013c858e335d7e24b0881b32677c0051341b0
gmerino92 <merino.gonza@gmail.com>
2018-05-15 04:15:03 -0300

    First approach at Friendship Requests UI flow.

--- app/src/main/java/stories/app/activities/FriendshipRequestsSentActivity.java
+++ app/src/main/java/stories/app/activities/FriendshipRequestsSentActivity.java
@@ -1,21 +1,20 @@
 package stories.app.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import stories.app.R;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class FriendshipRequestsSentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_friendship_requests_sent);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
@@ -24,18 +23,18 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_friendship_requests, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.profile) {
-            Intent navigationIntent = new Intent(HomeActivity.this, ProfileActivity.class);
+        if (item.getItemId() == R.id.search_friends) {
+            Intent navigationIntent = new Intent(FriendshipRequestsSentActivity.this, FriendshipRequestsActivity.class);
             startActivity(navigationIntent);
             return(true);
-        } else if (item.getItemId() == R.id.friendship_requests) {
-            Intent navigationIntent = new Intent(HomeActivity.this, FriendshipRequestsActivity.class);
+        } else if (item.getItemId() == R.id.friendship_requests_received) {
+            Intent navigationIntent = new Intent(FriendshipRequestsSentActivity.this, FriendshipRequestsReceivedActivity.class);
             startActivity(navigationIntent);
             return(true);
         }

aba920708e25662e70ce93b5349bc44d7418f26c
nico <nicotruk@gmail.com>
2018-05-21 12:56:01 -0300

    messages activity correcion con commit del appServer

--- app/src/main/java/stories/app/adapters/DirectMessagesAdapter.java
+++ app/src/main/java/stories/app/adapters/DirectMessagesAdapter.java
@@ -30,7 +30,14 @@ public class DirectMessagesAdapter extends ArrayAdapter<Message> {
             Message message = this.getItem(position);
 
             TextView messageUsername = (TextView) convertView.findViewById(R.id.message_item_title);
-            messageUsername.setText(message.from_username);
+            StringBuilder sb = new StringBuilder();
+            if(message._id.equals(message.from_username)) {
+                sb.append("From: ");
+            } else {
+                sb.append("To: ");
+            }
+            sb.append(message._id);
+            messageUsername.setText(sb.toString());
 
             TextView timestamp = (TextView) convertView.findViewById(R.id.message_item_timestamp);
             timestamp.setText(new SimpleDateFormat(DATE_FORMAT).format(new Date(message.timestamp)));

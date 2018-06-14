1817083264b67bf18ca49977ae0fac26eb5fe41e
gmerino92 <merino.gonza@gmail.com>
2018-06-14 03:20:58 -0300

    New styling for CreateStoryActivity.

--- app/src/main/java/stories/app/activities/CreateStoryActivity.java
+++ app/src/main/java/stories/app/activities/CreateStoryActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -39,6 +40,11 @@ public class CreateStoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_story);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        setTitle("Crear Historia");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         Button createStoryButton = this.findViewById(R.id.createStoryButton);
         createStoryButton.setOnClickListener(new CreateStoryHandler());

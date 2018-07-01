c316988937402d0de69cdc037d9e275d74451f1d
nanovazquez <marianodvazquez@gmail.com>
2018-07-01 12:43:31 -0300

    Use app server URI

--- app/src/main/java/stories/app/utils/Constants.java
+++ app/src/main/java/stories/app/utils/Constants.java
@@ -2,6 +2,6 @@ package stories.app.utils;
 
 public class Constants {
 
-    // public static final String appServerURI = "https://app-server-stories.herokuapp.com/api/v1";
-    public static final String appServerURI = "http://192.168.0.5:8000/api/v1";
+    public static final String appServerURI = "https://app-server-stories.herokuapp.com/api/v1";
+    //public static final String appServerURI = "http://192.168.0.5:8000/api/v1";
 }
